fb78fc62ec03f94fd5fd5a710baa10972acab4b7
jisoooo17 <jisoooo17@gmail.com>
2024-09-27 23:10:27 +0900

    jquery와 dataset 활용한 tab

--- tab/tab.js
+++ tab/tab.js
@@ -7,33 +7,46 @@ document.addEventListener("DOMContentLoaded", () => {
   // 모든 div에 붙은 show 클래스 제거
   // div 0에 show 클래스 추가
 
+  // forEach 사용
   const tabBtn = document.querySelectorAll(".tab-button");
   const tabCont = document.querySelectorAll(".tab-content");
 
-  tabBtn.forEach((item, index) => {
-    item.addEventListener("click", function(){
-      tabBtn.forEach(btn => {
-        btn.classList.remove("orange")
-      });
+  // tabBtn.forEach((item, index) => {
+  //   item.addEventListener("click", function(){
+  //     tabBtn.forEach(btn => {
+  //       btn.classList.remove("orange")
+  //     });
       
-      tabCont.forEach(cont => {
-        cont.classList.remove("show")
-      });
+  //     tabCont.forEach(cont => {
+  //       cont.classList.remove("show")
+  //     });
 
-      item.classList.add("orange");
-      tabCont[index].classList.add("show")
-    });
-  });
-
-
-  for(var i = 0; i < 3; i++){
-    console.log(213)
-  }
-
-  // document.querySelectorAll(".tab-button")[0].addEventListener("click", function(){
-  //   document.querySelectorAll(".tab-button")[0].classList.add("orange");
-  //   document.querySelectorAll(".tab-content")[0].classList.add("show");
+  //     item.classList.add("orange");
+  //     tabCont[index].classList.add("show")
+  //   });
   // });
 
+
+
+
+  // dataset 활용
+  document.querySelector(".list").addEventListener("click", function(e){
+    openTab(e.target.dataset.id);
+    console.log(e.target.dataset.id)
+  });
+
+  function openTab(index) {
+    tabBtn.forEach((btn) => {
+      btn.classList.remove("orange");
+    });
+
+    tabCont.forEach((cont) => {
+      cont.classList.remove("show");
+    });
+
+    // index를 사용하여 올바른 탭과 콘텐츠에 클래스 추가
+    tabBtn[index].classList.add("orange");
+    tabCont[index].classList.add("show");
+  }
 
 });
