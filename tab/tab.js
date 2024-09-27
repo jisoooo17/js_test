581e4e0a282c333c1033c7cf2addc5af08341452
jisoooo17 <jisoooo17@gmail.com>
2024-09-27 23:16:35 +0900

    dataset을 활용한  탭 기능

--- tab/tab.js
+++ tab/tab.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
   // 버틍 0 누르면
   // 모든 버튼에 붙은 orange 클래스 제거 
   // 버튼 0에 orange클래스 추가
   // 모든 div에 붙은 show 클래스 제거
   // div 0에 show 클래스 추가
 
-  // forEach 사용
+  // forEach 사용 ver
   const tabBtn = document.querySelectorAll(".tab-button");
   const tabCont = document.querySelectorAll(".tab-content");
 
@@ -27,9 +26,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // });
 
 
-
-
-  // dataset 활용
+  
+  // dataset 활용 ver
   document.querySelector(".list").addEventListener("click", function(e){
     openTab(e.target.dataset.id);
     console.log(e.target.dataset.id)
